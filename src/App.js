6799bf8b38c8b2449faa81e90836eed2bc53d88c
Chris Cresswell <Christorper@gmail.com>
2022-07-24 23:46:05 -0700

    Tweak layout

--- src/App.js
+++ src/App.js
@@ -19,8 +19,6 @@ class App extends Component {
       },
       schools: [
         { institution: "some school", range: "date - date", editable: false },
-        { institution: "some school", range: "date - date", editable: false },
-        { institution: "some school", range: "date - date", editable: false },
       ],
       companies: [
         {
