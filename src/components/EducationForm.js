165a24e179e8bb16db12e32a199fdb2851e63ef6
Chris Cresswell <Christorper@gmail.com>
2022-08-03 22:21:44 -0700

    Change EducationForm.js from class based to functional component

--- src/components/EducationForm.js
+++ src/components/EducationForm.js
@@ -1,39 +1,36 @@
 import React, { Component } from "react";
 import { BsCheckCircleFill } from "react-icons/bs";
 
-class EducationForm extends Component {
-  render() {
-    const { institution, range } = this.props.school;
-    const { index } = this.props;
-    const { handlesubmit, handlechange } = this.props;
-    return (
-      <form onSubmit={handlesubmit} className="education-form">
-        <label htmlFor="school">School</label>
-        <input
-          id="institution"
-          type="text"
-          index={index}
-          value={institution}
-          onChange={handlechange}
-          required
-          placeholder="a school you attended"
-        ></input>
-        <label htmlFor="range">Dates</label>
-        <input
-          id="range"
-          type="text"
-          index={index}
-          value={range}
-          onChange={handlechange}
-          required
-          placeholder="how long you attended"
-        ></input>
-        <button type="submit">
-          <BsCheckCircleFill size="1.5em" className="submit-icon" />
-        </button>
-      </form>
-    );
-  }
-}
+const EducationForm = () => {
+  const { institution, range } = this.props.school;
+  const { index, handlesubmit, handlechange } = this.props;
+  return (
+    <form onSubmit={handlesubmit} className="education-form">
+      <label htmlFor="school">School</label>
+      <input
+        id="institution"
+        type="text"
+        index={index}
+        value={institution}
+        onChange={handlechange}
+        required
+        placeholder="a school you attended"
+      ></input>
+      <label htmlFor="range">Dates</label>
+      <input
+        id="range"
+        type="text"
+        index={index}
+        value={range}
+        onChange={handlechange}
+        required
+        placeholder="how long you attended"
+      ></input>
+      <button type="submit">
+        <BsCheckCircleFill size="1.5em" className="submit-icon" />
+      </button>
+    </form>
+  );
+};
 
 export default EducationForm;
